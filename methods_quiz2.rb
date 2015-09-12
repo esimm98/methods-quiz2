0408cb9fbc04d46994c121ea7e689da1081df2bb
esimm98 <esimm98@gmail.com>
2015-09-11 19:51:22 -0600

    3 completed tests and methods transferred to forked file

--- methods_quiz2.rb
+++ methods_quiz2.rb
@@ -1,5 +1,57 @@
-module MethodsQuiz
+module MethodsQuiz2
 	
-	
+	def dice_sum(a, b)
+		without_doubles(a, b) ? add_non_doubles(a, b) : add_doubles(a, b) 
+	end
+
+	def max_maybe(a, b)
+		if (a == b)
+			return 0
+		elsif (a % 5 == b % 5)
+			return_min(a, b)
+		else
+			return_max(a, b)
+		end
+	end
+
+	def squirrels_play?(temp, summer)
+		temp >= 60 && (temp <= 90 && !summer) || (temp <= 100 && summer)
+	end
+
+	private
+
+		def without_doubles(a, b)
+			a != b ? true : false
+		end
+
+		def add_non_doubles(a, b)
+			return a + b
+		end
+
+		def add_doubles(a, b)
+			if a == 6
+				a = 1
+			else
+				a += 1
+			end
+			return a + b
+		end
+
+
+		def return_max(a, b)
+			if (a > b)
+				return a
+			else
+				return b
+			end
+		end
+
+		def return_min(a, b)
+			if (a < b)
+				return a
+			else
+				return b
+			end
+		end
 
 end
--- methods_quiz2_test.rb
+++ methods_quiz2_test.rb
@@ -4,13 +4,31 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative 'methods_quiz2'
 
-class MethodsQuizTest < MiniTest::Test
+class MethodsQuiz2Test < MiniTest::Test
 	def setup
 		@m = Class.new do
      include MethodsQuiz2
    	end.new
 	end
 
-	# TODO - write tests here
+	def test_dice_sum
+		assert_equal 8, @m.dice_sum(3, 5)
+		assert_equal 7, @m.dice_sum(6, 6)
+		assert_equal 11, @m.dice_sum(6, 5)
+		assert_equal 3, @m.dice_sum(1, 1)
+	end
+
+	def test_max_maybe
+		assert_equal 0, @m.max_maybe(1, 1)
+		assert_equal 7, @m.max_maybe(12, 7)
+		assert_equal 11, @m.max_maybe(11, 7)
+	end
+
+	def test_squirrels_play
+		assert_equal true, @m.squirrels_play?(60, false)
+		assert_equal true, @m.squirrels_play?(95, true)
+		assert_equal false, @m.squirrels_play?(105, true)
+		assert_equal false, @m.squirrels_play?(55, false)
+	end
 
 end
